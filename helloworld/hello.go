2a4fefa176db0ba5674e3f9a0863ffa9904c4282
Mark Isham <mark.isham@rigor.com>
2018-12-08 20:43:56 -0500

    lessons  Please enter the commit message for your changes. Lines starting

--- helloworld/hello.go
+++ helloworld/hello.go
@@ -1,7 +1,74 @@
 package main
 
-import "fmt"
+// See https://tour.golang.org/list
+
+import (
+"fmt"
+"strconv"
+)
+
+type MyStuff struct {
+	name string
+	value int
+}
+
+func PrintMyStuff(m MyStuff) {
+	fmt.Printf(m.name + ": " + strconv.Itoa(m.value) + "\n")
+}
+
+func addStuff(a int,b int) (int,string) {
+	return a+b,"Behold!"
+}
+
+func noChangeStuff(m MyStuff) {
+	// Pass by copy, no change
+	m.name="NOCHANGE"
+}
+
+func doChangeStuff(m *MyStuff) {
+	// Pass by ref, yes change
+	m.name="NEWVALUE"
+}
+
+func PlayWithStructs() {
+	fmt.Printf("\nStructs!\n")
+
+	addedVal,response := addStuff(5,3)
+	mystuff:=MyStuff { response, addedVal }
+	PrintMyStuff(mystuff)
+
+	noChangeStuff(mystuff)
+	PrintMyStuff(mystuff)
+
+	doChangeStuff(&mystuff)
+	PrintMyStuff(mystuff)
+}
+
+func printSlice(s []int) {
+	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
+}
+
+
+func PlayWithArrays() {
+	fmt.Printf("\nArrays!\n")
+	var myslice []int
+	printSlice(myslice)
+	myslice=append(myslice,123)
+	printSlice(myslice)
+	myslice=append(myslice,12345)
+	printSlice(myslice)
+
+	fmt.Printf("\nOutputing the array\n")
+	for i, v := range myslice {
+		fmt.Printf("index %d = %d\n", i, v)
+	}
+}
 
 func main() {
-	fmt.Printf("hello, world\n")
+	PlayWithStructs()
+	PlayWithArrays()
+
+	// Continue here: https://tour.golang.org/moretypes/19
+
 }
+
