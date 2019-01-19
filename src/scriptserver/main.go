7ae1184ca084d86653240a8a1ac301fc646edb2a
Mark Isham <mark.isham@rigor.com>
2019-01-19 13:17:06 -0500

    query string

--- src/scriptserver/main.go
+++ src/scriptserver/main.go
@@ -6,20 +6,25 @@ import (
 	"net/http"
 )
 
+var version string
+
 func htmlBody(w http.ResponseWriter, r *http.Request) {
 	str := "<html><head><script src=\"script1.js\"></script></head><body><h1>Hello There</h1></body></html>"
 	fmt.Fprintf(w, str)
+
+	queryValues := r.URL.Query()
+	vqc := queryValues.Get("v")
+	if vqc != "" {
+		version = vqc
+	}
 }
 
 func script1(w http.ResponseWriter, r *http.Request) {
-	// TODO...
-	vers := r.URL.Query().Get("v")
-
-	str := "console.log(\"script 1 loading\");"
-	str += "//" + vers + "\n"
+	str := "console.log(\"script 1 loading...\");"
+	str += "//" + version + "\n"
 	str += "var s=document.createElement(\"script\");"
 
-	if vers == "3" {
+	if version == "3" {
 		str += "s.setAttribute(\"src\",\"script3.js\");"
 	} else {
 		str += "s.setAttribute(\"src\",\"script2.js\");"
@@ -48,11 +53,12 @@ func script3(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	version = "1"
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", htmlBody)
 	mux.HandleFunc("/script1.js", script1)
 	mux.HandleFunc("/script2.js", script2)
 	mux.HandleFunc("/script3.js", script3)
 
-	log.Fatal(http.ListenAndServe(":8080", mux))
+	log.Fatal(http.ListenAndServe(":8081", mux))
 }
